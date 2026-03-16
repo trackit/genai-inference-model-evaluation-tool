67675ba4f9df9b47a7b4d1aef952d4bf2c763f25
DaoudAA <ahmedaziz.daoud@gmail.com>
2026-03-16 13:31:22 +0100

    chore: fix frontend formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 function App() {
-    const [count, setCount] = useState(0);
-    return (<>
+  const [count, setCount] = useState(0);
+  return (
+    <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo"/>
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo"/>
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -25,6 +26,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>);
+    </>
+  );
 }
 export default App;
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 const queryClient = new QueryClient();
-createRoot(document.getElementById('root')).render(<StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </StrictMode>);
+  </StrictMode>,
+);
